369650a63d7cb7228c716d0d391b1f86c9334fb5
Crowdbotics <engineering@crowdbotics.com>
2023-07-07 05:27:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-leaf-dev-98560",
+  url: "https://twilight-leaf-dev-98560.botics.co",
+  api: "https://twilight-leaf-dev-98560.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
